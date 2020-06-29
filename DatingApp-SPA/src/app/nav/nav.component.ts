5c952cd69639b7c70f21f3eed8c2918b4e191e8c
Francis-K-Lau <francis.k.lau@gmail.com>
2020-06-29 15:21:15 -0700

    End of Section 7

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AltertifyService } from '../_services/altertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +11,8 @@ import { AltertifyService } from '../_services/altertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AltertifyService) { }
+  constructor(public authService: AuthService, private alertify: AltertifyService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,7 +24,10 @@ export class NavComponent implements OnInit {
     }, error => {
 //      console.log('Failed to login');
       this.alertify.error(error);
-    });
+    }, () => {
+      this.router.navigate(['/members']);
+    }
+    );
   }
 
   loggedIn() {
@@ -35,6 +40,7 @@ export class NavComponent implements OnInit {
   logOut() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
